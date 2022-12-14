11e1b16a5b4bf0683c9cd18bfb93b6fb2e581c99
Eric-Showers <ejs1357@hotmail.com>
2022-12-14 14:34:02 -0800

    Ces't fini

--- 12/12.py
+++ 12/12.py
@@ -1,3 +1,85 @@
+class Node:
+    def __init__(self, position, parent, g, f, height):
+        self.position = position
+        self.state = f"{position[0]},{position[1]}"
+        self.parent = parent
+        self.height = height
+        self.path_cost = g
+        self.f = f
+
+
+def montreal_distance(position1, position2):
+    # Calculate the Toronto distance to the goal.
+    return abs(position1[0] - position2[0]) + abs(position1[1] - position2[1])
+
+
+def solution(node, initial_node):
+    # Create a list of nodes in the path.
+    path = [node]
+    # While the current node is not the start node.
+    while node != initial_node:
+        # Add the parent of the current node to the path.
+        path.append(node.parent)
+        # Set the current node to the parent of the current node.
+        node = node.parent
+    return path
+
+
+def eh_star_search(height_map, start_pos, goal_pos):
+    actions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+    # Create a priority queue.
+    pq = []
+    # Add the start node to the queue.
+    start_node = Node(
+        start_pos, 
+        None, 
+        0, 
+        montreal_distance(start_pos, goal_pos), 
+        height_map[start_pos[0]][start_pos[1]]
+    )
+    pq.append(start_node)
+    # Create dicts of visited nodes, node parents, f, and g values.
+    explored = {}
+    # While the queue is not empty.
+    while len(pq) > 0:
+        # Remove first node from pq with the highest f value.
+        cur_node = pq.pop(0)
+        # If the current node is the goal node.
+        if cur_node.position == goal_pos:
+            # Return a list of nodes in the path.
+            return solution(cur_node, start_node)
+        # Check if current node is unexplored, or if this is a better path.
+        if cur_node.state not in explored or explored[cur_node.state] > cur_node.path_cost:
+            explored[cur_node.state] = cur_node.path_cost
+            # For each action in the list of actions.
+            for action in actions:
+                # Check if the node is in bounds and a legal move from current position
+                new_node_pos = [
+                    cur_node.position[0] + action[0], 
+                    cur_node.position[1] + action[1]
+                ]
+                if (
+                    new_node_pos[0] < 0 
+                    or new_node_pos[0] >= len(height_map) 
+                    or new_node_pos[1] < 0 
+                    or new_node_pos[1] >= len(height_map[0])
+                ):
+                    continue
+                if height_map[new_node_pos[0]][new_node_pos[1]] - cur_node.height > 1:
+                    continue
+                # Create a new node.
+                new_node = Node(
+                    new_node_pos, 
+                    cur_node, 
+                    cur_node.path_cost + 1, 
+                    montreal_distance(new_node_pos, goal_pos) + cur_node.path_cost + 1, 
+                    height_map[new_node_pos[0]][new_node_pos[1]]
+                )
+                # Add the new node to the queue.
+                pq = sorted(pq+[new_node], key=lambda x: x.f)
+    return False
+
+
 file_data = open("input.txt").read().splitlines()
 # Create 2d array of height values from ord(a) to ord(z).
 # (0,0) is top left, (-1,0) is bottom left.
@@ -15,89 +97,16 @@ for y, line in enumerate(file_data):
             height_map[x][y] = ord(char) - ord('a')
 
 
-def print_map(h_map, positions):
-    for y in range(len(h_map[0])):
-        for x in range(len(h_map)):
-            if [x, y] in positions:
-                print("#", end="")
-            else:
-                print(file_data[y][x], end="")
-        print()
+# Remove one from path length because start node not counted.
+shortest_hike = len(eh_star_search(height_map, start, goal)) - 1
+print('Part 1:', shortest_hike)
+for y in range(len(height_map[0])):
+    for x in range(len(height_map)):
+        if height_map[x][y] == 0:
+            hike_path = eh_star_search(height_map, [x, y], goal)
+            if hike_path:
+                if len(hike_path)-1 < shortest_hike:
+                    shortest_hike = len(hike_path)-1
 
 
-# Eh star search algorithm.
-
-
-def montreal_distance(position1, position2):
-    # Calculate the Toronto distance to the goal.
-    return abs(position1[0] - position2[0]) + abs(position1[1] - position2[1])
-
-
-def prioritize_queue(queue, f):
-    # Prioritize the queue.
-    return sorted(queue, key=lambda x: f[0])
-
-
-actions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-
-# Create a priority queue.
-pq = []
-# Add the start node to the queue.
-pq.append((0, start))
-# Create dicts of visited nodes, node parents, f, and g values.
-visited = {}
-parent = {}
-f = {}
-g = {}
-# Add the start node to the visited dictionary.
-visited[f"{start[0]},{start[1]}"] = 0
-# Add the start node to the parent dictionary.
-parent[f"{start[0]},{start[1]}"] = None
-# Add the start node to the g dictionary.
-g[f"{start[0]},{start[1]}"] = 0
-# Add the start node to the f dictionary.
-f[f"{start[0]},{start[1]}"] = montreal_distance(start, goal)
-# While the queue is not empty.
-while len(pq) > 0:
-    # Remove first node from pq with the highest f value.
-    current = pq.pop()[1]
-    curString = f"{current[0]},{current[1]}"
-    # If the current node is the goal node.
-    if current == goal:
-        # Create a list of nodes in the path.
-        path = [current]
-        # While the current node is not the start node.
-        while current != start:
-            # Add the parent of the current node to the path.
-            path.append(parent[f"{current[0]},{current[1]}"])
-            # Set the current node to the parent of the current node.
-            current = parent[f"{current[0]},{current[1]}"]
-        # Return the path.
-        print(len(path)-1)
-        break
-    # For each action in the list of actions.
-    for action in actions:
-        # Calculate the new node.
-        new_node = [current[0] + action[0], current[1] + action[1]]
-        # Check if the new node is in bounds.
-        if new_node[0] < 0 or new_node[0] >= len(height_map) or new_node[1] < 0 or new_node[1] >= len(height_map[0]):
-            continue
-        # Check if the new node is at most one higher
-        if height_map[new_node[0]][new_node[1]] - height_map[current[0]][current[1]] > 1:
-            continue
-        # If the new node is not in the visited dictionary.
-        pos_str = f"{new_node[0]},{new_node[1]}"
-        if pos_str not in visited or visited[pos_str] > g[curString]+1:
-            # Add the new node to the parent dictionary.
-            parent[pos_str] = current
-            # Add the new node to the g dictionary.
-            if pos_str not in g:
-                g[pos_str] = g[curString]
-            g[pos_str] += 1
-            # Add the new node to the visited dictionary.
-            visited[pos_str] = g[pos_str]
-            # Add the new node to the f dictionary.
-            f.update({pos_str: g[pos_str] + montreal_distance(new_node, goal)})
-            # Add the new node to the queue.
-            pq.append((f[pos_str], new_node))
-            pq.sort(key=lambda x: x[0])
+print('Part 2: ', shortest_hike)
